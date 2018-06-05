a0f539d717a2a8e729291911d6507d90b10e0585
Dangal2018 <35136867+Dangal2018@users.noreply.github.com>
2018-06-05 11:50:17 +0800

    求和，求平均数

---
+++ sum and average.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#define N 10
+int average()
+{
+	int i,a[N],sum=0;
+	float average=0;
+	for(i=0;i<N;i++){
+		scanf("%d",&a[i]);
+	sum=sum+a[i];
+	average=1.*sum/N;}
+	printf("sum: %d average: %.1f ",sum,average);
+}
+int main()
+{
+	int a[N];
+	average();
+	return 0;
+}
