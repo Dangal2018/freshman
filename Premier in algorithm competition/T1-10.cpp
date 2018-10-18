aeb88e8c18ab07892370cd0cbdd0eaff1f122cbf
Dangal2018 <35136867+Dangal2018@users.noreply.github.com>
2018-10-18 20:54:42 +0800

    Create T1-10.cpp

---
+++ Premier in algorithm competition/T1-10.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+int main()
+{
+    int year;
+    std::cin>>year;
+    if(((year%4==0&&year%100!=0)||year%400==0))
+    std::cout<<"yes";
+    else std::cout<<"no";
+    return 0;
+}
